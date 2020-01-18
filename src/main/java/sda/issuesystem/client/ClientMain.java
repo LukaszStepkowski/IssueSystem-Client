5b694ba31d750e66e3828b3433290fc6b5426699
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-01-18 10:30:17 +0100

    implemented printing main menu and created inifinite loop for menu choice

--- src/main/java/sda/issuesystem/client/ClientMain.java
+++ src/main/java/sda/issuesystem/client/ClientMain.java
@@ -1,8 +1,25 @@
 package sda.issuesystem.client;
 
+import sda.issuesystem.client.menu.MainMenuParams;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ClientMain {
 
     public static void main(String[] args) {
-        System.out.println("Start of the project");
+
+        for (int i = 0; i < MainMenuParams.values().length; i++) {
+            System.out.println(MainMenuParams.values()[i].getCommandNumber() + ". " + MainMenuParams.values()[i].getCommandDescription());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int choice = scanner.nextInt();
+            Arrays.stream(MainMenuParams.values())
+                    .filter(p -> p.getCommandNumber() == choice)
+                    .forEach(p -> System.out.println(p.getCommandDescription()));
+        }
     }
 }
