435b56b89f3677893cd3906e42ea3ee826bf5a91
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-01-18 10:32:27 +0100

    created abstract class AbstractMenu

---
+++ src/main/java/sda/issuesystem/client/menu/AbstractMenu.java
@@ -0,0 +1,16 @@
+package sda.issuesystem.client.menu;
+
+public abstract class AbstractMenu {
+
+    MainMenuParams params;
+
+    public AbstractMenu(MainMenuParams params) {
+        this.params = params;
+    }
+
+    public void setParams(MainMenuParams params) {
+        this.params = params;
+    }
+
+    public abstract void printMenu();
+}
