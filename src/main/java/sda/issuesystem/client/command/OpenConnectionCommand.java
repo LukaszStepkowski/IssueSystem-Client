ead764aee8970f52ee6da4928639f1dedeac4f85
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-01-19 14:08:06 +0100

    created OpenConnectionCommand

---
+++ src/main/java/sda/issuesystem/client/command/OpenConnectionCommand.java
@@ -0,0 +1,26 @@
+package sda.issuesystem.client.command;
+
+import sda.issuesystem.client.config.ConnectionConfiguration;
+import sda.issuesystem.client.menu.command.CommandInterface;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class OpenConnectionCommand implements CommandInterface<Void> {
+
+    @Override
+    public void execute(Void onObject) {
+        try {
+        Socket channel = new Socket(ConnectionConfiguration.getAddress(), ConnectionConfiguration.getPort());
+            ObjectOutputStream out = new ObjectOutputStream(channel.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(channel.getInputStream());
+            out.writeObject("Message sent test");
+            out.flush();
+            System.out.println(in.readObject());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
